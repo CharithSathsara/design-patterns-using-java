0d1cffff859b10191691e335914a627c2bb9d72c
Charith Sathsara <charithsathsara169@gmail.com>
2022-09-21 18:34:45 +0530

    Commit Facade Pattern-Update

--- src/lk/charithsathsara/facadepattern/FacadePatternTest.java
+++ src/lk/charithsathsara/facadepattern/FacadePatternTest.java
@@ -19,7 +19,6 @@ public class FacadePatternTest {
         //generating MySql HTML report and Oracle PDF report using Facade
         HelperFacade.generateReport(HelperFacade.DBTypes.MYSQL, HelperFacade.ReportTypes.HTML, tableName);
         HelperFacade.generateReport(HelperFacade.DBTypes.ORACLE, HelperFacade.ReportTypes.PDF, tableName);
-
 	}
 
 }
