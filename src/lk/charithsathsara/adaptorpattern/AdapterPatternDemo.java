6e2b86e44fa4a52620975a5426c32d09666415dc
Charith Sathsara <charithsathsara169@gmail.com>
2022-09-21 18:26:30 +0530

    Commit Adapter Pattern Change

--- src/lk/charithsathsara/adaptorpattern/AdapterPatternDemo.java
+++ src/lk/charithsathsara/adaptorpattern/AdapterPatternDemo.java
@@ -2,6 +2,7 @@ package lk.charithsathsara.adaptorpattern;
 
 public class AdapterPatternDemo {
 	public static void main(String[] args) {
+
 	      AudioPlayer audioPlayer = new AudioPlayer();
 
 	      audioPlayer.play("mp3", "beyond the horizon.mp3");
