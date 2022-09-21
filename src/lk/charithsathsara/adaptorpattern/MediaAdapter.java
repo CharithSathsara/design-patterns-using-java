d9de86b3ef26812d2a331a01dfb0a2b148cd5e53
Charith Sathsara <charithsathsara169@gmail.com>
2022-09-21 18:29:31 +0530

    Commit Adapter Pattern-Update

--- src/lk/charithsathsara/adaptorpattern/MediaAdapter.java
+++ src/lk/charithsathsara/adaptorpattern/MediaAdapter.java
@@ -16,12 +16,13 @@ public class MediaAdapter implements MediaPlayer{
 
 	@Override
 	public void play(String audioType, String fileName) {
+
 		if(audioType.equalsIgnoreCase("vlc")){
-	         advancedMusicPlayer.playVlc(fileName);
-	      }
-	      else if(audioType.equalsIgnoreCase("mp4")){
-	         advancedMusicPlayer.playMp4(fileName);
-	      }
+			advancedMusicPlayer.playVlc(fileName);
+		}
+		else if(audioType.equalsIgnoreCase("mp4")){
+			advancedMusicPlayer.playMp4(fileName);
+		}
 		
 	}
 
